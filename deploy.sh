38581933c9e31769d204d996081b93ddb1748c23
Sandy Liao <sandy@Sandy.local>
2022-05-10 20:40:03 +0800

    [Added] github pages

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# 當發生錯誤時終止腳本運行
+set -e
+
+# 打包
+yarn build
+
+# 移動至到打包後的dist目錄 
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://github.com/sandy0120/schart.git 分支為 gh-pages
+git push -f https://github.com/sandy0120/chart.git master:gh-pages
+cd -
