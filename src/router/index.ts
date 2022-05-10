5093655978c1795dc42a87e7bafd6d23d6d6796c
Sandy Liao <sandy@Sandy.local>
2022-05-10 20:46:08 +0800

    [Fixed] router

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,7 +31,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('chart'),
   routes
 })
 
